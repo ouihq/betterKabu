9394ba5a672844648859f1288dec3e26972b3e0e
davbauer <nuro.clexx78@outlook.de>
2022-07-05 09:31:48 +0200

    fix manifest

---
+++ init.js
@@ -0,0 +1,8 @@
+// Weiterleiten zu Stundenplan nicht Termine
+if ((document.referrer == "https://www.digikabu.de") || (document.referrer == "https://www.digikabu.de/Main/TestRedirect")) {
+    window.location.href = "https://www.digikabu.de/Stundenplan/Klasse";
+}
+
+if ((document.referrer == "https://digikabu.de") || (document.referrer == "https://digikabu.de/Main/TestRedirect")) {
+    window.location.href = "https://digikabu.de/Stundenplan/Klasse";
+}
